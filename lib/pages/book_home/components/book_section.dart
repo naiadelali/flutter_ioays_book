e7adc41a69782e4daea488999e674b2a5b68c453
wagnermagnus1 <wagnerserafa@gmail.com>
2021-06-18 09:53:29 -0300

    Ajustes de merge

--- lib/pages/book_home/components/book_section.dart
+++ lib/pages/book_home/components/book_section.dart
@@ -28,7 +28,10 @@ class BookSection extends StatelessWidget {
               crossAxisSpacing: 10,
               mainAxisSpacing: 110,
               crossAxisCount: 2,
-              children: List.generate(10, buildBookContainer),
+              children: List.generate(
+                10,
+                (index) => buildBookContainer(index, context),
+              ),
             ),
           ),
         )
@@ -36,55 +39,64 @@ class BookSection extends StatelessWidget {
     );
   }
 
-  Widget buildBookContainer(int index) => Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: kWhiteColor,
-              border: Border.all(color: kWhiteColor),
-              borderRadius: BorderRadius.all(
-                Radius.circular(30),
+  Widget buildBookContainer(int index, BuildContext context) => GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => BookDetailsPage(),
+              ));
+        },
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: kWhiteColor,
+                border: Border.all(color: kWhiteColor),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'A Culpa é das Estrelas',
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: kprimaryColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Jonh Green',
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: kGrayColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'A Culpa é das Estrelas',
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: kprimaryColor,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16,
-                  ),
+            Container(
+              transform: Matrix4.translationValues(0, -80, 0),
+              child: Center(
+                child: Image.network(
+                  'https://files-books.ioasys.com.br/Book-0.jpg',
+                  height: 150,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Jonh Green',
-                  maxLines: 1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: kGrayColor,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            transform: Matrix4.translationValues(0, -80, 0),
-            child: Center(
-              child: Image.network(
-                'https://files-books.ioasys.com.br/Book-0.jpg',
-                height: 150,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
 }
